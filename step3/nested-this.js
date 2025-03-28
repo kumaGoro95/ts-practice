f263041535bb4f2b57e9af8aeb3c26ace2853a8d
kumaGoro95 <todira.island@gmail.com>
2025-03-28 19:19:03 +0900

    jsのthisの仕組みを確認

---
+++ step3/nested-this.js
@@ -0,0 +1,14 @@
+class Person {
+constructor(name) {
+    this.name = name;
+}
+
+greet() {
+    function doIt() {
+        console.log(`Hi I'm ${this.name}`);
+    }
+    doIt();
+}
+}
+const minky = new Person('momo');
+minky.greet();
