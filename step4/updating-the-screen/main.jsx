1017673b17824beb112102d58fc63bb9135f7b2c
kumaGoro95 <todira.island@gmail.com>
2025-04-23 19:41:01 +0900

    コメント追加

--- step4/updating-the-screen/main.jsx
+++ step4/updating-the-screen/main.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MyApp from './App';
 
+// デフォルトコンポーネントをどこに表示するかを定義している
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<MyApp />);
